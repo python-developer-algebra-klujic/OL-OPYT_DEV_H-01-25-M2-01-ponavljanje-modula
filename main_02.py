22faa87fb0c5975a48689673fbe59b05b96c2fc6
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-28 18:42:10 +0100

    Update main_02.py

--- main_02.py
+++ main_02.py
@@ -1,11 +1,17 @@
 # Datoteke .txt i .json
 
+content = 'Lorem ipsum'
+text = [
+    'Lorem ipsum',
+    'Dolor sit',
+    'Jos malo teksta'
+]
 
 # Kod za pisanje u datoteku
 try:
     with open('students.txt', 'a')as file_writer:
-        file_writer.write()
-        file_writer.writelines()
+        file_writer.write(content)
+        file_writer.writelines(text)
 
 except Exception as ex:
     print(f'Dogodila se greska {ex}')
@@ -14,9 +20,9 @@ except Exception as ex:
 # Kod za citanje iz datoteke
 try:
     with open('students.txt', 'r')as file_reader:
-        file_reader.read()
-        file_reader.readline()
-        file_reader.readlines()
+        file_content = file_reader.read()
+        file_content = file_reader.readline()
+        file_content_list = file_reader.readlines()
 
 except Exception as ex:
     print(f'Dogodila se greska {ex}')
