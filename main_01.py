3b4acb9b4205536f6e4811a8df9a9af8ff18b408
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-28 18:27:11 +0100

    Update main_01.py

--- main_01.py
+++ main_01.py
@@ -6,6 +6,16 @@ last_name_pero = 'Peric'
 year_of_birth_pero = 1999
 average_grade_pero = 4.89
 is_student_pero = True
+university_pero = 'Sveuciliste Rijeka'
+
+first_name_iva = 'Iva'
+last_name_iva = 'Ivic'
+year_of_birth_iva = 2001
+average_grade_iva = 4.98
+is_student_iva = True
+university_iva = 'Sveuciliste Rijeka'
+
+
 
 # rjecnik student
 student_pero = {
@@ -15,3 +25,18 @@ student_pero = {
     'average_grade': 4.89,
     'is_student': True
 }
+student_pero['university'] = 'Sveuciliste Rijeka'
+
+student_iva = {
+    'first_name': 'Iva',
+    'last_name': 'Ivic',
+    'year_of_birth': 2001,
+    'average_grade': 4.98,
+    'is_student': True
+}
+student_iva['university'] = 'Sveuciliste Rijeka'
+
+students = []
+students.append(student_pero)
+students.append(student_iva)
+
