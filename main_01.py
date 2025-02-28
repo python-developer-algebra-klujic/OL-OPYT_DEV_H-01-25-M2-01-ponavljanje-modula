4fb21f2c86dcc25dced23cfdab8f681f7751d8b5
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-28 18:17:15 +0100

    Create main_01.py

---
+++ main_01.py
@@ -0,0 +1,17 @@
+# Varijable i kolekcije podataka
+
+# Cuvanje podataka o studentici/u
+first_name_pero = 'Pero'
+last_name_pero = 'Peric'
+year_of_birth_pero = 1999
+average_grade_pero = 4.89
+is_student_pero = True
+
+# rjecnik student
+student_pero = {
+    'first_name': 'Pero',
+    'last_name': 'Peric',
+    'year_of_birth': 1999,
+    'average_grade': 4.89,
+    'is_student': True
+}
